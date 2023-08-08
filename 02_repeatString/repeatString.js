d288272ecfe079e30e11521c9f6f6f5c17265a44
Volodymyr <volodymyr.pechenevskyi@gmail.com>
2023-08-08 16:36:29 +0200

    complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    
+    newString = string.repeat(num);
+    return newString;
 };
 
 // Do not edit below this line

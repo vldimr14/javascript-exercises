4de9a905365c3c4fd975f2ba8b9b51b3c5fdae66
Volodymyr <volodymyr.pechenevskyi@gmail.com>
2023-08-08 17:38:54 +0200

    complete the 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    const newArray = [];
 
+    array.forEach((element) => {
+        if (!values.includes(element)) {
+            newArray.push(element);
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line

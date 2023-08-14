25822642a3b8898bdf98c62a1e8dc67fd5dfc36a
Volodymyr <volodymyr.pechenevskyi@gmail.com>
2023-08-14 14:28:45 +0200

    complete the 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum = 0;
 
+    if (!(typeof min === "number") || !(typeof max === "number")) {
+        return "ERROR";
+    }
+
+    if (min < 0 || max < 0) return "ERROR";
+
+    if (min > max) {
+        let temp = min;
+        min = max;
+        max = temp;
+    }
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

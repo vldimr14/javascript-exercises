af6379b0ab0adeaff9cf26c64a8a1d3ff3ce445d
Volodymyr <volodymyr.pechenevskyi@gmail.com>
2023-08-31 15:12:07 +0200

    complete the 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(fibIndex) {
+    if (fibIndex < 0) return 'OOPS';
 
+    let fibSequence = [0, 1];
+
+    for (let i = 2; i < fibIndex + 1; i++) {
+        fibSequence[i] = fibSequence[i - 2] + fibSequence[i - 1]; 
+    }
+
+    return fibSequence[fibIndex];
 };
 
 // Do not edit below this line

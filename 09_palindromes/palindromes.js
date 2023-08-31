9c115eeff7925d98bbba577d3a63809bb3583e1f
Volodymyr <volodymyr.pechenevskyi@gmail.com>
2023-08-31 14:57:12 +0200

    complete the 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleanString = Array.from(string).filter((val) => val !== '.' && val !== '!' && val !== ',' && val !== ' ');
+    let cleanStringCopy = cleanString.slice();
 
+    return cleanString.reverse().join('').toLowerCase() === cleanStringCopy.join('').toLowerCase();
 };
 
 // Do not edit below this line

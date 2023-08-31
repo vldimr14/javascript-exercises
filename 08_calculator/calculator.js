eb7e88515b26affb60587d540dd0a328da75637d
Volodymyr <volodymyr.pechenevskyi@gmail.com>
2023-08-31 14:07:06 +0200

    complete the 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	return args.length > 0 ? args.reduce((acc , cur) => acc + cur) : 0;
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((acc, cur) => acc * cur);
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return Math.pow(num, pow);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line

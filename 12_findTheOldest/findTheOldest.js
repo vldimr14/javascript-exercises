6b7f6bf47039e57bcdd3c997a3cdf09d984c54d6
Volodymyr <volodymyr.pechenevskyi@gmail.com>
2023-09-01 11:33:24 +0200

    complete the 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((prev, curr) => {
+        const date = new Date(Date.now());
+        if (prev.yearOfDeath === undefined) {
+            prev.yearOfDeath = date.getFullYear();
+        } else if (curr.yearOfDeath === undefined) {
+            curr.yearOfDeath = date.getFullYear();
+        }
 
+        return (prev.yearOfDeath - prev.yearOfBirth) > (curr.yearOfDeath - curr.yearOfBirth) ? prev : curr
+    });
 };
 
 // Do not edit below this line
